061955e85b6ee48a440484ab8269ea541d536699
Grigoriy <Grigoriy.Plotnikov.GDC@ts.fujitsu.com>
2022-10-17 23:19:19 +0300

    Fix hot reloads TT

---
+++ GeoData/GeoApp/watch.config.js
@@ -0,0 +1,5 @@
+﻿const path = require('path');
+const common = require('./webpack.config.js');
+
+common.output.path = path.resolve(__dirname, '../bin/Debug/net5.0/wwwroot/js');
+module.exports = common;
