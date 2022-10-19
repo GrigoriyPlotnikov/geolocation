c9d496b65df12b690a656b7c3bdbc8ecb266d2b6
Grigoriy <Grigoriy.Plotnikov.GDC@ts.fujitsu.com>
2022-10-19 12:35:12 +0300

    Jest tests

---
+++ GeoData/GeoApp/tst/component.test.js
@@ -0,0 +1,11 @@
+﻿import { createElement } from '../src/component.js'
+
+it('renders without crashing', async () => {
+  const div = document.createElement('div');
+  /** @jsx createElement */
+  const Subj = () => (
+    <span />
+  );
+  div.appendChild(<Subj />);
+  await new Promise(resolve => setTimeout(resolve, 1000));
+});
--- GeoData/GeoApp/watch.config.js
+++ GeoData/GeoApp/watch.config.js
@@ -1,5 +1,10 @@
-﻿const path = require('path');
-const common = require('./webpack.config.js');
+﻿import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import common from './webpack.config.js';
 common.output.path = path.resolve(__dirname, '../bin/Debug/net5.0/wwwroot/js');
-module.exports = common;
+
+export default common;
